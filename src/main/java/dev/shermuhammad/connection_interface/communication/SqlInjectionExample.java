a5330a7c72576c92eec3ad629483e86911d6b7ad
Muhammad Karimov <karimov.shermukhammad.tech@gmail.com>
2026-06-05 20:14:50 +0500

    test commit

---
+++ src/main/java/dev/shermuhammad/connection_interface/communication/SqlInjectionExample.java
@@ -0,0 +1,7 @@
+package dev.shermuhammad.connection_interface.communication;
+
+public class SqlInjectionExample {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/test/java/dev/shermuhammad/Test.java
@@ -0,0 +1,4 @@
+package dev.shermuhammad;
+
+public class Test {
+}
